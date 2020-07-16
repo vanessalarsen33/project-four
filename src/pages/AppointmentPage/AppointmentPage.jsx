1f5bec94083f3f3e1fdae83db5d94ad8c8a5be6d
vanessalarsen33 <vanessalarsen33@gmail.com>
2020-07-16 16:46:03 -0600

    form update

--- src/pages/AppointmentPage/AppointmentPage.jsx
+++ src/pages/AppointmentPage/AppointmentPage.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
@@ -7,115 +7,121 @@ import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import './AppointmentForm.css';
 
+// class AddAppointmentPage extends Component {
+//   state = {
+//     formData: {
+//       date: '',
+//       service: '',
+//       notes: ''
+//     }
+//   }
 
-class AddAppointmentPage extends Component {
-  state = {
-    formData: {
-      date: '',
-      service: '',
-      notes: ''
+//   handleChange = e => {
+//     const formDataAsUserTypes = {
+//       ...this.state.formData,
+//       [e.target.name]: e.target.value
+//     }
+
+//     this.setState({
+//       formData: formDataAsUserTypes
+//     })
+//   }
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     this.props.handleAddAppointent(this.state.formData);
+//   };
+
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
     },
-    service: '',
-    open: false,
-  };
-  
-  //  styles = makeStyles((theme) => ({
-  //   container: {
-  //     display: 'flex',
-  //     flexWrap: 'wrap',
-  //   },
-  //   textField: {
-  //     marginLeft: theme.spacing(1),
-  //     marginRight: theme.spacing(1),
-  //     width: 200,
-  //   },
-  //   button: {
-  //     display: 'block',
-  //     marginTop: theme.spacing(2),
-  //   },
-  //   formControl: {
-  //     margin: theme.spacing(1),
-  //     minWidth: 120,
-  //   },
-  // }));
-  // classes = this.styles();
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+    button: {
+      display: 'block',
+      marginTop: theme.spacing(2),
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+  }));
 
+export default function DateAndTimePickers() {
+  const classes = useStyles();
+  const [service, setService] = React.useState('');
+  const [formData, setFormData] = React.useState({date: '', service: '', notes: ''});
+  const [open, setOpen] = React.useState(false);
 
-  handleChange = e => {
-    const formDataAsUserTypes = {
-      ...this.state.formData,
-      [e.target.name]: e.target.value,
-    }
-    const service = e.target.value
-
-    this.setState({
-      formData: formDataAsUserTypes,
-      service: service
+  const handleChange = (event) => {
+    // setService(event.target.value);
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
     })
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    //create function in app js pass function down. pass it formData.
   }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.handleAddAppointent(this.state.formData);
-  };
-
-
-  handleClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
-
-  handleOpen = () => {
-    this.setState({
-      open: true,
-    });
-  };
-
-  render() {
-    return (
-      <form className="appointmentForm">
-        <h3>DATE</h3>
-        <TextField
-          id="datetime-local"
-          type="datetime-local"
-          defaultValue="2017-05-24T10:30"
-          value={this.state.formData.date}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <h3>SERVICE</h3>
-        <Select
-          labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
-          open={this.open}
-          onClose={this.handleClose}
-          onOpen={this.handleOpen}
-          value={this.state.formData.service}
-          onChange={this.handleChange}
-        >
-          <MenuItem>
-          </MenuItem>
-          <MenuItem>Massage</MenuItem>
-          <MenuItem>Hair cut</MenuItem>
-          <MenuItem>Facial</MenuItem>
-          <MenuItem>Pedicure</MenuItem>
-          <MenuItem>Manicure</MenuItem>
-          <MenuItem>Facial</MenuItem>
-        </Select>
-        <h3>NOTES:</h3>
-        <TextField
-        value={this.state.formData.notes}
-
-        />
-        <Button
-          type="submit"
-        >
-          SUBMIT</Button>
-      </form>
-    );
-  }
+  return (
+    <form className="appointmentForm">
+      <h3>DATE</h3>
+      <TextField
+        id="datetime-local"
+        type="datetime-local"
+        className={classes.textField}
+        value={formData.date}
+        onChange={handleChange}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        name="date"
+      />
+      <h3>SERVICE</h3>
+      <Select
+        labelId="demo-controlled-open-select-label"
+        id="demo-controlled-open-select"
+        open={open}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        value={formData.service}
+        onChange={handleChange}
+        name="service"
+      >
+        <MenuItem>
+        </MenuItem>
+        <MenuItem value="massage">Massage</MenuItem>
+        <MenuItem value="haircut">Hair cut</MenuItem>
+        <MenuItem value="facial">Facial</MenuItem>
+        <MenuItem value="pedicure">Pedicure</MenuItem>
+        <MenuItem value="manicure">Manicure</MenuItem>
+      </Select>
+      <h3>NOTES:</h3>
+      <TextField 
+        name="notes"
+        value={formData.notes}
+        onChange={handleChange}
+      />
+      <Button
+        type="submit"
+      >
+        SUBMIT</Button>
+    </form>
+  );
 }
-
-export default AddAppointmentPage;
